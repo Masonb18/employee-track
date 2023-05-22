7cd244124517be8082346725616df9e4ba9b662d
Masonb18 <bauer.mason01@gmail.com>
2023-05-22 12:18:48 -0500

    inputing the choices for the inquirer prompt

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ inquirer
 .prompt([
     {
         type: 'list',
+        name: 'action',
+        message: 'Please select what you want to do',
+        choices:[
+            'view all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+        ]
     }
 ])
 
